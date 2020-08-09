1e7fcf30a208954d6e33a9318435a61c4274788f
Mantas Kantautas <mantas.kantautas@gmail.com>
2020-08-09 21:11:21 +0300

    fixing an edge case where users technically were able to navigate manually to a auth protected page. :bug:

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\User as UserResource;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Request;
 
 class LoginController extends Controller
 {
--- frontend/middleware/user.js
+++ frontend/middleware/user.js
@@ -1,4 +1,7 @@
 export default function (context) {
+  if (context.$auth.error) {
+    context.redirect('/login')
+  }
   context.$axios.onError((error) => {
     if ([403, 401, 419].includes(error.response.status)) {
       context.redirect('/login')
